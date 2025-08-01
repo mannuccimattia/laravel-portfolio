dc7e1f7b57d00d2f8e0ac9d39a2e33ba325ea7f3
mannuccimattia <mannuccimattia@gmail.com>
2025-08-01 14:49:51 +0200

    [FEAT] Added amin page

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin-layout')
+
+@section('title')
+    Index | Admin
+@endsection
+
+@section('content')
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Admin Page') }}
+        </h2>
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">{{ __('Admin Dashboard') }}</div>
+
+                    <div class="card-body">
+                        <div>
+                            <strong>admin:</strong> {{ $user->name }}
+                        </div>
+                        <div>
+                            <strong>mail:</strong> {{ $user->email }}
+                        </div>
+                        <div>
+                            <strong>mreated at:</strong> {{ $user->created_at }}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
