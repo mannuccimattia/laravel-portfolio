866e6743d011bca1fd534240d1e99d2a92b1809f
mannuccimattia <mannuccimattia@gmail.com>
2025-08-04 13:05:01 +0200

    [UPD] Done projects index

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,12 +5,45 @@
 @endsection
 
 @section('content')
-    <h1>My projects</h1>
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                {{ $project->name }}
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+
+        <h1>My projects</h1>
+
+        <div class="">
+            <div class="row">
+                <div class="col fw-bold">
+                    Name
+                </div>
+                <div class="col fw-bold">
+                    Client
+                </div>
+                <div class="col fw-bold">
+                    Date
+                </div>
+                <div class="col fw-bold">
+                    Link
+                </div>
+            </div>
+
+
+            @foreach ($projects as $project)
+                <div class="row">
+                    <div class="col">
+                        {{ $project->name }}
+                    </div>
+                    <div class="col">
+                        {{ $project->client }}
+                    </div>
+                    <div class="col">
+                        {{ $project->date }}
+                    </div>
+                    <div class="col">
+                        link
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+
+    </div>
 @endsection
