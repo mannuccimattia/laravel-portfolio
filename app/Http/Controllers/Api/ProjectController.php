d6d87eabd49631bb64f838be0c3720bc03ad9e4e
mannuccimattia <mannuccimattia@gmail.com>
2025-08-12 14:43:40 +0200

    [UPD] Done api index and show route

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::with("type", "technologies")->get();
+
+        // dd($projects);
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $projects
+            ]
+        );
+    }
+
+    public function show(Project $project)
+    {
+        $project->load("type", "technologies");
+
+        return response()->json([
+            "success" => true,
+            "ddata" => $project
+        ]);
+    }
 }
