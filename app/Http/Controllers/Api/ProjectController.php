0b55e7a70460efe7bfd40863b8cc21fd39b8031c
mannuccimattia <mannuccimattia@gmail.com>
2025-08-25 13:00:29 +0200

    [FIX] Fixed project show response: typo in key "data"

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -28,7 +28,7 @@ class ProjectController extends Controller
 
         return response()->json([
             "success" => true,
-            "ddata" => $project
+            "data" => $project
         ]);
     }
 }
